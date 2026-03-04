6aefeff9e8eb6ed358e2dfdc95d2991636937db6
VICTOR HUGO CERQUEIRA SILVA <25162461@edu119.local>
2026-03-04 16:49:42 -0300

    Firts Commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import usuarioRoutes from "./routes/usuarioRoutes.js"
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res)=>{
+    res.json({msg: "Hello world"})
+})
+
+app.use("/usuarios", usuarioRoutes)
+export default app
